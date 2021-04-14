3a44d0f3a327609b35a9d08e31a1cfa5c7681d2b
Natan <natan-moreira1@hotmail.com>
2021-04-14 20:58:05 -0300

    feat: Config axios

--- super_show/src/routes/index.tsx
+++ super_show/src/routes/index.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Cart from '../components/Cart'
 import Header from '../components/Header'
 // import ProductDetail from '../components/ProductDetail'
 // import Products from '../components/Products'
+import api from '../services/api'
 
 const Routes = () => {
+  useEffect(() => {
+    const getItems = async () => {
+      try {
+        console.log(process.env.REACT_APP_API_URL)
+        const { data } = await api.get('')
+        console.log(data)
+      } catch (e) {
+        console.log(e)
+      }
+    }
+    getItems()
+  }, [])
+
   return (
     <Router>
       <Header />
