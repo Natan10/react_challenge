60f487ce7cad1760d3f376dd5d64599083fa317b
Natan <natan-moreira1@hotmail.com>
2021-04-16 09:15:17 -0300

    chore: Install react-toastify

--- super_show/src/App.tsx
+++ super_show/src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import Routes from './routes'
+import { Slide, ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   return (
     <>
       <Routes />
+      <ToastContainer
+        position="bottom-right"
+        autoClose={6000}
+        closeOnClick
+        transition={Slide}
+        newestOnTop={false}
+        hideProgressBar={false}
+      />
     </>
   )
 }
