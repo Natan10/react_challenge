84da7e411f59a99981077dd9f38bee044ae4ad22
Natan <natan-moreira1@hotmail.com>
2021-04-16 08:10:16 -0300

    refactor: Add Buttons in Cart

--- super_show/src/components/CartItem/index.tsx
+++ super_show/src/components/CartItem/index.tsx
@@ -1,21 +1,19 @@
-/* eslint-disable no-unused-vars */
 import React, { useState } from 'react'
-import { ButtonGroup, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
-import { GoPlus } from 'react-icons/go'
-import { FiMinus } from 'react-icons/fi'
-import { AiOutlineCloseCircle } from 'react-icons/ai'
-
 import {
   addProductToCartSaga,
-  updateProductInCartSaga
+  updateProductInCartSaga,
+  removeProductToCartSaga
 } from '../../store/cart/action'
+import { AiOutlineCloseCircle } from 'react-icons/ai'
+import { totalValue, convertValue } from '../../util'
 import './index.scss'
+import Buttons from '../Buttons'
 
 const CartItem = ({ product }: any) => {
-  const [quantity, setQuantity] = useState(product?.qtd)
   const dispatch = useDispatch()
   const { cartReducer } = useSelector((state): any => state)
+  const [quantity, setQuantity] = useState(product?.qtd)
 
   const index = cartReducer.products.findIndex(
     (prod: any) => prod.id === product.id
@@ -33,18 +31,16 @@ const CartItem = ({ product }: any) => {
   }
 
   const handleRemoveItem = () => {
-    if (quantity !== 0) {
+    if (quantity > 0) {
       dispatch(updateProductInCartSaga(product))
       setQuantity(quantity - 1)
     } else {
-      new Notification('Olá', {
-        body: 'Não pode mais tirar!!!'
-      })
+      handleRemoveProductToCart()
     }
   }
 
   const handleRemoveProductToCart = () => {
-    console.log('kapapakp')
+    dispatch(removeProductToCartSaga(product.id))
   }
 
   return (
@@ -56,34 +52,31 @@ const CartItem = ({ product }: any) => {
         <p className="carditem_description">{product.description}</p>
       </td>
       <td>
-        <ButtonGroup size="sm" className="cartitem_buttons">
-          <Button
-            className="cartitem_increase"
-            variant="outline-primary"
-            onClick={handleAddItem}
-          >
-            <GoPlus />
-          </Button>
-          <Button className="cartitem_quantity" disabled>
-            {quantity}
-          </Button>
-          <Button
-            className="cartitem_decrease"
-            variant="outline-primary"
-            onClick={handleRemoveItem}
-          >
-            <FiMinus />
-          </Button>
-        </ButtonGroup>
+        <Buttons
+          add={handleAddItem}
+          remove={handleRemoveItem}
+          quantity={quantity}
+        />
       </td>
       <td>
-        <span className="cartitem_value_unit">R$ 100,00</span>
+        <span className="cartitem_value_unit">
+          {product.offer
+            ? convertValue(product.offer)
+            : convertValue(product.price)}
+        </span>
       </td>
       <td>
-        <span className="cartitem_value_total">R$ 250,00</span>
+        <span className="cartitem_value_total">
+          {totalValue(product, quantity)}
+        </span>
       </td>
       <td>
-        <AiOutlineCloseCircle size={25} onClick={handleRemoveProductToCart} />
+        <div
+          className="cart_remove_product"
+          onClick={handleRemoveProductToCart}
+        >
+          <AiOutlineCloseCircle size={25} />
+        </div>
       </td>
     </tr>
   )
