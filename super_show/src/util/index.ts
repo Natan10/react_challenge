69bce259c00de0e6e51ed6f126d38e788608887c
Natan <natan-moreira1@hotmail.com>
2021-04-16 00:24:36 -0300

    feat: Create functions for calc values

--- super_show/src/util/index.ts
+++ super_show/src/util/index.ts
@@ -1,3 +1,5 @@
+import { CartData } from '../store/cart/types'
+
 export const convertValue = (value: number): string => {
   return value.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
 }
@@ -18,4 +20,44 @@ export const convertTextPromo = (promotion: PromoType) => {
     promotion.value - promotion.base
   } a mais de graça!!!`
   return textPromo
+}
+
+export const totalValue = (product: CartData, quantity: number) => {
+  if (product.offer && product.promotion) {
+    if (quantity >= product.promotion.base) {
+      const count = checkPromo(quantity, product.promotion.value)
+      return convertValue((quantity - count) * product.offer)
+    }
+  } else if (product.promotion) {
+    const count = checkPromo(quantity, product.promotion.value)
+    return convertValue((quantity - count) * product.price)
+  } else if (product.offer) {
+    return convertValue(quantity * product.offer)
+  }
+  return convertValue(product.price * quantity)
+}
+
+export const sumValue = (product: CartData, quantity: number) => {
+  if (product.offer && product.promotion) {
+    if (quantity >= product.promotion.base) {
+      const count = checkPromo(quantity, product.promotion.value)
+      return (quantity - count) * product.offer
+    }
+  } else if (product.promotion) {
+    const count = checkPromo(quantity, product.promotion.value)
+    return (quantity - count) * product.price
+  } else if (product.offer) {
+    return quantity * product.offer
+  }
+  return product.price * quantity
+}
+
+const checkPromo = (quantity: number, value: number) => {
+  let count = 0
+  let aux = quantity
+  while (aux >= value) {
+    aux -= value
+    count += 1
+  }
+  return count
 }
