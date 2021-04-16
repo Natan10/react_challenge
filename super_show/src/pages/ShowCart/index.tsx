5d568171acab106b894a74bc7eeb7bd90d105764
Natan <natan-moreira1@hotmail.com>
2021-04-16 00:25:15 -0300

    feat: Create ShowCart page

--- super_show/src/pages/ShowCart/index.tsx
+++ super_show/src/pages/ShowCart/index.tsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import Cart from '../../components/Cart'
 
 const ShowCart = () => {
-  return <h1>Cart</h1>
+  const [cart, setCart] = useState([])
+  const { cartReducer } = useSelector((state): any => state)
+
+  useEffect(() => {
+    setCart(cartReducer.cart)
+  }, [cartReducer.cart])
+
+  return <Cart products={cart} />
 }
 export default ShowCart
