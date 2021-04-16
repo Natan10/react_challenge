778acd698feb8f6792cde0317da76f0c653d9e52
Natan <natan-moreira1@hotmail.com>
2021-04-16 08:56:21 -0300

    refactor: Change style Cart

--- super_show/src/components/Cart/index.tsx
+++ super_show/src/components/Cart/index.tsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container, Table } from 'react-bootstrap'
+import { useSelector } from 'react-redux'
+import { sumValue, convertValue } from '../../util'
 import CardItem from '../CartItem'
+import EmptyCart from '../EmptyCart'
 import { ProductProps } from '../Product'
 import './index.scss'
 
@@ -9,25 +12,52 @@ interface CartProducts {
 }
 
 const Cart = ({ products }: CartProducts) => {
+  const [total, setTotal] = useState(0)
+
+  const { cartReducer } = useSelector((state): any => state)
+  const cart = useSelector((state): any => state)
+
+  const value = cartReducer.cart.reduce((count: number, product: any) => {
+    const quantity = product.qtd
+    return (count += sumValue(product, quantity))
+  }, 0)
+
+  console.log(cart)
   return (
     <Container className="carditem_container">
-      <Table borderless>
-        <thead>
-          <tr>
-            <th>Produto</th>
-            <th>Descrição</th>
-            <th>Quantidade</th>
-            <th>Valor Unitário</th>
-            <th>Valor Total</th>
-            <th>Remover Do Carrinho</th>
-          </tr>
-        </thead>
-        <tbody>
-          {products.map((prod: any) => {
-            return <CardItem key={prod.id} product={prod} />
-          })}
-        </tbody>
-      </Table>
+      {products.length > 0 ? (
+        <Table borderless>
+          <thead>
+            <tr>
+              <th>Produto</th>
+              <th>Descrição</th>
+              <th>Quantidade</th>
+              <th>Valor Unitário</th>
+              <th>Valor Total</th>
+              <th>Remover Do Carrinho</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((prod: any) => {
+              return (
+                <CardItem
+                  key={prod.id}
+                  product={prod}
+                  calcTotal={setTotal}
+                  total={total}
+                />
+              )
+            })}
+            <tr className="total_cart">
+              <td>Total</td>
+              <td colSpan={4}></td>
+              <td>{convertValue(value)}</td>
+            </tr>
+          </tbody>
+        </Table>
+      ) : (
+        <EmptyCart />
+      )}
     </Container>
   )
 }
