ecde37fdfdd71b791e49491d2bb0418dcf9cdbf0
Natan <natan-moreira1@hotmail.com>
2021-04-16 12:03:19 -0300

    refactor: Change store

--- super_show/src/store/index.ts
+++ super_show/src/store/index.ts
@@ -1,7 +1,6 @@
 import { createStore, applyMiddleware, Middleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import createSagaMiddleware from 'redux-saga'
-
 import rootReducer from './rootReducer'
 import rootSaga from './rootSaga'
 
